170585d564e0bb6be6b9fa8df22064f9cb42c431
Jose <josederomero27@gmail.com>
2026-04-03 11:32:28 -0500

    Avance-Jose

--- ProyectoCurso/src/gui/V1.java
+++ ProyectoCurso/src/gui/V1.java
@@ -12,6 +12,7 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class V1 extends JFrame implements ActionListener {
 
@@ -24,6 +25,7 @@ public class V1 extends JFrame implements ActionListener {
 	private JTextField txtDNI;
 	private JButton btnNewButton_1;
 	private JLabel lblNewLabel_2;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -53,48 +55,56 @@ public class V1 extends JFrame implements ActionListener {
 		contentPane.setLayout(null);
 		{
 			lblNewLabel = new JLabel("Nombre: ");
-			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			lblNewLabel.setBounds(10, 15, 81, 22);
+			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
 			contentPane.add(lblNewLabel);
 		}
 		{
 			txtNombre = new JTextField();
-			txtNombre.setBounds(70, 19, 96, 18);
+			txtNombre.setBounds(80, 19, 96, 18);
 			contentPane.add(txtNombre);
 			txtNombre.setColumns(10);
 		}
 		{
 			btnNewButton = new JButton("Adicionar");
-			btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			btnNewButton.setForeground(new Color(0, 0, 0));
+			btnNewButton.setBackground(new Color(0, 0, 0));
 			btnNewButton.setBounds(31, 91, 112, 29);
+			btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 			contentPane.add(btnNewButton);
 		}
 		{
 			lblNewLabel_1 = new JLabel("DNI:");
-			lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			lblNewLabel_1.setBounds(199, 18, 44, 17);
+			lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
 			contentPane.add(lblNewLabel_1);
 		}
 		{
 			txtDNI = new JTextField();
-			txtDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			txtDNI.setBounds(242, 17, 96, 18);
+			txtDNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			contentPane.add(txtDNI);
 			txtDNI.setColumns(10);
 		}
 		{
 			btnNewButton_1 = new JButton("Eliminar");
+			btnNewButton_1.setBackground(new Color(0, 0, 0));
+			btnNewButton_1.setBounds(166, 91, 112, 28);
 			btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 			btnNewButton_1.addActionListener(this);
-			btnNewButton_1.setBounds(166, 91, 112, 28);
 			contentPane.add(btnNewButton_1);
 		}
 		{
 			lblNewLabel_2 = new JLabel("Teléfono");
-			lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			lblNewLabel_2.setBounds(10, 48, 81, 22);
+			lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 14));
 			contentPane.add(lblNewLabel_2);
 		}
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(80, 48, 96, 18);
+		contentPane.add(textField);
 
 	}
 	public void actionPerformed(ActionEvent e) {
